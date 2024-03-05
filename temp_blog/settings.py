696ca122e8f0581d9917c0c1948259f9ebfd4a5a
Mukul_kYadav <ymukul6180@gmail.com>
2024-03-06 00:00:43 +0530

    password reset

--- temp_blog/settings.py
+++ temp_blog/settings.py
@@ -139,3 +139,11 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGOUT_REDIRECT_URL = 'login'
 
 LOGIN_URL = 'login'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('E_ID')
+EMAIL_HOST_PASSWORD = os.environ.get('E_PASS')
